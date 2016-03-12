1a80df18f46fc15b7677638afb9404ed34586133
Robert Layton <robertlayton@gmail.com>
2016-03-12 11:10:12 +1100

    Flask app, and will only roar max of once every 5 seconds

--- roar.py
+++ roar.py
@@ -1,14 +1,52 @@
 import os
 import logging
+from datetime import datetime
 from slacker import Slacker
+from flask import Flask
 
-token = os.getenv('TREX')
-print(token)
-if token is None:
-    logging.error("You'll need an API access token, and ensure it is set to the environment variable TREX")
-    exit(1)
+app = Flask(__name__)
 
-slack = Slacker(token=token)
+slack = None
 
-# Send a message to #general channel
-slack.chat.post_message('#random', 'ROAR!', as_user=True, link_names=1)
+g = {}
+
+MIN_ROAR_INTERVAL = 5  # Do not roar if we roared within X seconds
+
+@app.route('/roar')
+def hello_world():
+    global slack, g
+    if not slack:
+        setup()
+    # Send a message to #random channel
+    now = datetime.now()
+
+    print(g)
+
+    is_set = False
+    if "last_roared" not in g:
+        g['last_roared'] = now
+        is_set = True
+
+    interval = (now - g['last_roared']).seconds
+    if  interval >= MIN_ROAR_INTERVAL:
+        #slack.chat.post_message('#random', 'ROAR!', as_user=True, link_names=1)
+        g['last_roared'] = now
+        is_set = True
+        return "Roared: {} ({}s since) is_set={}".format(g['last_roared'], interval, is_set)
+    else:
+        return "Too quick: {} ({}s since) is_set={}".format(g['last_roared'], interval, is_set)
+
+
+def setup():
+    global slack
+    token = os.getenv('TREX')
+    print(token)
+    if token is None:
+        logging.error("You'll need an API access token, and ensure it is set to the environment variable TREX")
+        exit(1)
+    slack = Slacker(token=token)
+
+
+if __name__ == "__main__":
+    setup()
+    app.run(host='0.0.0.0', debug=True)
